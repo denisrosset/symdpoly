8525daa6c4de3b0ae5835414f9d40ff78ae98e98
Denis Rosset <physics@denisrosset.com>
2019-03-10 19:31:11 -0400

    Rich involution ops.

--- core/src/main/scala/net/alasc/symdpoly/syntax/package.scala
+++ core/src/main/scala/net/alasc/symdpoly/syntax/package.scala
@@ -2,6 +2,8 @@ package net.alasc.symdpoly
 
 package object syntax {
 
+  object richInvolution extends RichInvolutionSyntax
+
   object phased extends PhasedSyntax
 
   object all extends AllSyntax
