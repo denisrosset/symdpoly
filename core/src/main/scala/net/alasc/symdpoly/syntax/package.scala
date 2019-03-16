813650b7a3636351c4bca31ac6642fb37b386c50
Denis Rosset <physics@denisrosset.com>
2019-03-16 04:07:32 -0400

    Removed rich involution ops.

--- core/src/main/scala/net/alasc/symdpoly/syntax/package.scala
+++ core/src/main/scala/net/alasc/symdpoly/syntax/package.scala
@@ -2,8 +2,6 @@ package net.alasc.symdpoly
 
 package object syntax {
 
-  object richInvolution extends RichInvolutionSyntax
-
   object phased extends PhasedSyntax
 
   object all extends AllSyntax
