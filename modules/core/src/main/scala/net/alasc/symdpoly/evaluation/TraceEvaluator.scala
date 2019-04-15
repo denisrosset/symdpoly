b513912aeca986e5c3299ed463ec8e08724304c4
Denis Rosset <physics@denisrosset.com>
2019-04-15 11:26:28 -0400

    Fixed trace evaluator.

--- modules/core/src/main/scala/net/alasc/symdpoly/evaluation/TraceEvaluator.scala
+++ modules/core/src/main/scala/net/alasc/symdpoly/evaluation/TraceEvaluator.scala
@@ -2,25 +2,24 @@ package net.alasc.symdpoly
 package evaluation
 
 import scala.collection.immutable.HashSet
-
 import shapeless.Witness
 import spire.syntax.action._
 import spire.syntax.involution._
 import spire.syntax.cfor._
-
 import syntax.phased._
 import net.alasc.finite.Grp
 import net.alasc.symdpoly.generic.SingleMoment
 import net.alasc.symdpoly.math.{GrpDecomposition, Phase}
 import spire.syntax.std.seq._
-
 import net.alasc.perms.default._
 import net.alasc.symdpoly.free.MutableWord
+
+import scala.annotation.tailrec
 
 /** Equivalence under the adjoint operation. */
 final case class TraceEvaluator[
   M <: freebased.MonoidDef.Aux[F] with Singleton,
-  F <: free.MonoidDef.Aux[F]
+  F <: free.MonoidDef.Aux[F] with Singleton
 ](real: Boolean, symmetryGroup: Grp[M#PermutationType])(implicit val witnessMono: Witness.Aux[M]) extends Evaluator {
 
   implicit def witnessF: Witness.Aux[F] = valueOf[M].witnessFree
@@ -35,14 +34,17 @@ final case class TraceEvaluator[
 
   def allCyclicPermutations(mono: M#MonoType): Set[M#MonoType] =
     if (mono.data.length <= 1) Set(mono) else {
+      var rewritten = false
       val l = mono.data.length
-      Seq.tabulate(l) { i =>
+      val res = Seq.tabulate(l) { i =>
         val w = mono.data.mutableCopy
         cforRange(0 until l) { j =>
           w(j) = mono.data((i + j) % l)
         }
+        rewritten |= M.inPlaceNormalForm(w)
         new freebased.Mono[M, F](w.setImmutable())
       }.toSet
+      if (rewritten) res.flatMap(allCyclicPermutations) else res
   }
 
   def apply(mono: M#MonoType): SingleMomentType = {
