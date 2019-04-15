1baa4fb7dba973f6d21305855d06e6162cd9d52f
Denis Rosset <physics@denisrosset.com>
2019-04-15 19:16:06 -0400

    Clean up dead code.

--- modules/tests/src/test/scala/net/alasc/symdpoly/laws/Phases.scala
+++
@@ -1,9 +0,0 @@
-package net.alasc.symdpoly
-package laws
-
-import net.alasc.symdpoly.math.{Phase, Phases}
-import org.scalacheck.{Arbitrary, Gen}
-import org.typelevel.discipline.Predicate
-
-object Phaseses {
-}
